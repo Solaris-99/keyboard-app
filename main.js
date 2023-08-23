bf1272b8f02cb4fe9c53b85bf9d7d15946a8d665
Solaris-99 <e.cifuentes996@gmail.com>
2023-08-23 15:46:01 -0300

    rewrite of keyboard and websocket implementation

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 const back = require('androidjs').back;
 const net = require('net');
 
-const opts = {
+let opts = {
 	port: 8001,
-	host: '192.168.1.41'
+	host: 'localhost'
 }
 
 let connected = false
 let client = null
+back.on("IP",(msg) =>{
+	opts.host = msg
+	client = net.createConnection(opts)
+	console.log("connect_signal")
 
+	client.on('error', (err) => {
+		console.log(err.message)
+	})
+
+	client.on('connect', () => {
+		console.log(`successful connection host: ${opts['host']} port: ${opts['port']}`)
+		connected = true
+	})
+})
 
 back.on("btn", (msg) => {
 	console.log(msg)
-	
-	if (msg == "CONNECT"){
-		client = net.createConnection(opts)
-		console.log("connect_signal")
-	
-		client.on('error', (err) => {
-			console.log(err.message)
-		})
-	
-		client.on('connect', () => {
-			console.log(`successful connection host: ${opts['host']} port: ${opts['port']}`)
-			connected = true
-		})
-	}
-	else if (connected) {
+	if (connected) {
 		console.log("sending..")
 		client.write(msg)
 	}
